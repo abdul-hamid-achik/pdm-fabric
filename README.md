0c79dcc8f74c642be6039e29e8b328a059c4e8e5
abdul hamid <abdulachik@icloud.com>
2022-10-05 19:54:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# pdm-fabric: Fabric command shortcuts for PDM
+
+A plugin that gives you command shortcuts for developing with [PDM](https://pdm.fming.dev/) and
+[Fabric](https://www.fabfile.org).
+
+
+## Requirements
+
+Tested with PDM 2.1.X
+
+## Install
+
+`pdm plugin add pdm-fabric`
+
+## Usage
+
+### fab
+
+Example: `pdm fab shell --env dev`
+
+## Configuring .env
+
+pdm-django inherits from the `pdm run` command, so if you would like to load a dotenv file, use the same notation in your `pyproject.toml` file.
+
+```toml
+[tool.pdm.scripts]
+_.env_file = ".env"
+```
+
+## CLI Option Conflicts
+
+You may run into some options conflicts between the `pdm` command and the Django command you run. As of right now the only one I can find is `pdm manage --help` for which you will get the pdm help instead of the Django help. However, note you can still run `pdm manage` or `pdm manage help` to view the Django help. Same situation applies for `pdm django-admin --help`.
+
+## Shameless Plugs
+
+I built this library originally for the [NeutronSync Service](https://www.neutronsync.com/). So if you would like to support this project please support the service with a subscription to NeutronSync or a [donation](https://github.com/sponsors/neutron-sync) to the open source libraries.
---
+++ pdm_fabric/main.py
@@ -0,0 +1,45 @@
+import argparse
+import subprocess
+import sys
+
+from pdm.cli.utils import PdmFormatter
+from pdm.cli.commands.base import BaseCommand
+from pdm.cli.commands.run import Command as RunCommand
+from pdm.cli.commands.run import TaskRunner
+from pdm.cli.hooks import HookManager
+from pdm.cli.options import skip_option
+from pdm.cli.utils import check_project_file
+
+
+class FabricCommand(RunCommand):
+    OPTIONS = []
+    COMMAND_PREFIX = ['fab']
+
+    def add_arguments(self, parser):
+        skip_option.add_to_parser(parser)
+        parser.add_argument(
+            "args",
+            nargs=argparse.REMAINDER,
+            help="Arguments that will be passed to the command",
+        )
+
+    def handle(self, project, options):
+        options.list = False
+        options.command = self.COMMAND_PREFIX[0]
+
+        check_project_file(project)
+        hooks = HookManager(project, options.skip)
+        runner = TaskRunner(project, hooks=hooks)
+        if options.site_packages:
+            runner.global_options.update({"site_packages": options.site_packages})
+
+        sys.exit(runner.run(options.command, self.COMMAND_PREFIX[1:] + options.args))
+
+        hooks.try_emit("pre_run", script=options.command, args=options.args)
+        exit_code = runner.run(options.command, self.COMMAND_PREFIX[1:] + options.args)
+        hooks.try_emit("post_run", script=options.command, args=options.args)
+        sys.exit(exit_code)
+
+
+def reg_commands(core):
+    core.register_command(FabricCommand, "fab")
